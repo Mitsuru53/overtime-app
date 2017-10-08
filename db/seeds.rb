b7782fed7ebffed4985fd5c6d12fa3ed495cacda
Mitsuru Hashimoto <mitsuruhashimoto@mbp.local>
2017-10-08 15:37:50 +0900

    update seed file and finx index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+@user = User.create(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge', first_name: 'John', last_name: 'Snow')
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
